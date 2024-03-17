ea67c0269cc92723963ae939cd49c24fe5f71ef7
Sid1332 <69396556+Sid1332@users.noreply.github.com>
2024-03-17 11:53:30 -0400

    Update LimelightCommand.java

--- LimelightCommand.java
+++ LimelightCommand.java
@@ -7,7 +7,6 @@ import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
-import frc.robot.LimelightHelpers;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.LimelightSubsystem;
 
@@ -25,7 +24,8 @@ public class LimelightCommand extends Command {
 
     @Override
     public void initialize() {
-        CameraServer.startAutomaticCapture(new HttpCamera("limelight", "http://10.52.43.11:5800/", HttpCameraKind.kMJPGStreamer));
+        CameraServer.startAutomaticCapture(new HttpCamera(Constants.limelight1, "http://10.52.43.11:5800/", HttpCameraKind.kMJPGStreamer));
+        CameraServer.startAutomaticCapture(new HttpCamera(Constants.limelight2, "http://10.52.43.12:5800/", HttpCameraKind.kMJPGStreamer));
         CameraServer.startAutomaticCapture();
     }
 
@@ -33,17 +33,18 @@ public class LimelightCommand extends Command {
     public void execute() {
         NetworkTable table = NetworkTableInstance.getDefault().getTable(Constants.limelight1);
         // TODO: set button to available button.
-      if (Constants.mainStick.getRawButton(2)) {
-        m_limelightSubsystem.alignWithSpeaker(table);
-        m_driveSubsystem.diffDrive.arcadeDrive(m_limelightSubsystem.xSpeed, m_limelightSubsystem.forwardSpeed);
-      } //else {
+        m_limelightSubsystem.resetSpeeds();
+        if (Constants.mainStick.getRawButton(2)) {
+            m_limelightSubsystem.alignWithSpeaker(table);
+        } //else {
         // table.getEntry("ledMode").setNumber(0);
     //   }
         // TODO: set button to available button.
         if (Constants.mainStick.getRawButton(3)) {
             m_limelightSubsystem.alignWithAmp(table);
-            m_driveSubsystem.diffDrive.arcadeDrive(m_limelightSubsystem.xSpeed, 0);
         }
+        m_driveSubsystem.diffDrive.arcadeDrive(m_limelightSubsystem.xSpeed, m_limelightSubsystem.forwardSpeed);
+
       // System.out.println(LimelightHelpers.getBotPose(""));
       
     //   if (Constants.mainStick.getRawButton(1)) {
@@ -53,4 +54,6 @@ public class LimelightCommand extends Command {
     //     table.getEntry("ledMode").setNumber(0);
     //   }
     }
+
+    
 }
